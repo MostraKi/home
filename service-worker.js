1603ac5f6e20d81a83ccf81183ccfe927d482237
MostraKi <contato.mostraki@gmail.com>
2025-06-27 16:45:09 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,3 @@
+const CACHE_NAME = 'mostraki-cache-v1'; const urlsToCache = [ '/', '/index.html', '/fundo-desktop.png', '/fundo-mobile.png', '/logo-mostraki.png' ];
+self.addEventListener('install', event => { event.waitUntil( caches.open(CACHE_NAME) .then(cache => cache.addAll(urlsToCache)) ); });
+self.addEventListener('fetch', event => { event.respondWith( caches.match(event.request) .then(response => response || fetch(event.request)) ); });

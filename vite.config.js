cbd14bc05b0dbe7875ff012b4f5591f8a4820548
MostraKi <contato.mostraki@gmail.com>
2025-07-05 12:37:14 -0300

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  base: './', // <-- ESSENCIAL pro Vercel mostrar os arquivos direito
-  plugins: [react()],
-})

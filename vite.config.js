984312cd0a2a0df6bc7e0bb714ea719b4f2d0825
MostraKi <contato.mostraki@gmail.com>
2025-07-05 11:26:16 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: './', // <-- ESSENCIAL pro Vercel mostrar os arquivos direito
   plugins: [react()],
 })
